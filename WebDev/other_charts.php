2dddf2835939ddc0dcc0ffce012fd5984e019614
georgezervas17 <icsd13055@icsd.aegean.gr>
2017-05-29 19:10:46 +0300

    insert_project_other_charts

--- WebDev/other_charts.php
+++ WebDev/other_charts.php
@@ -19,8 +19,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-$sql = "SELECT * FROM projects ";
+//MESOS OROS VATHMOLOGIWN TWN DIPLWMATIKWN ANA KATHHGHTH
+$sql = "SELECT teacher,AVG(grade) as avg From projects WHERE status='complete' GROUP BY teacher ";
 $result = $conn->query($sql);
 ?>
 
@@ -28,30 +28,22 @@ $result = $conn->query($sql);
 
 
                 var data = new google.visualization.DataTable();
-                data.addColumn('string', 'Status');
-                data.addColumn('number', 'Percent');
+                data.addColumn('string', 'Teacher');
+                data.addColumn('number', 'AVG ');
 
 <?php
 $counter1 = 0;
 if ($result->num_rows > 0) {
     // output data of   each row
     while ($row = $result->fetch_assoc()) {
-        if ($row["teacher"] == 'Soras') {
-            $counter1++;
-            $teacher = $row["teacher"];
-        }
+        ?>
+                        data.addRows([
+                            ['<?php echo $row["teacher"]; ?>', <?php echo (float) $row["avg"]; ?>],
+                        ]);
+
+
+        <?php
     }
-    ?>
-                    data.addRows([
-                        ['<?php echo $teacher; ?>', <?php echo $counter1; ?>],
-                        ['Other', <?php
-    $other = $result->num_rows - $counter1;
-    echo $other;
-    ?>],
-                    ]);
-
-
-    <?php
 }
 $servername = "localhost";
 $username = "root";
@@ -61,6 +53,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+//PERISSOTERES DIPLWMATIKES ANA ETOS KAI SUNOLIKA
 $teacher = $_SESSION["username"];
 $sql2 = "SELECT teacher, YEAR(date_creation) as year,COUNT(*) as avg FROM `projects` GROUP BY teacher,YEAR(date_creation) ";
 $result2 = $conn->query($sql2);
@@ -72,25 +66,32 @@ $result2 = $conn->query($sql2);
 <?php
 $counter = 0;
 if ($result2->num_rows > 0) {
+    ?>
+                    var data12 = [];
+                    var Header = ['Year', 'Number of Projects', '', ''];
+                    data12.push(Header);
+    <?php
     // output data of   each row
     while ($row1 = $result2->fetch_assoc()) {
         ?>
-                        var data2 = google.visualization.arrayToDataTable([
-        <?php if ($counter == 0) { ?>
-                                ['Year', 'Sales', 'Expenses', 'Expenses'],
-                                ['<?php echo $row1["year"]; ?>', <?php echo (int) $row1["avg"]; ?>, 0, 0],
-        <?php } else { ?>
-                                ['<?php echo $row1["year"]; ?>', <?php echo (int) $row1["avg"]; ?>, 0, 0],
-        <?php } ?>
+                        var temp = [];
+                        temp.push('<?php echo $row1["year"]; ?>,<?php echo $row1["teacher"] ?>');
+                                temp.push(<?php echo (int) $row1["avg"]; ?>);
+                                temp.push(0);
+                                temp.push(0);
 
-                        ]);
+                                data12.push(temp);
 
         <?php
-        $counter++;
     }
 }
 ?>
+                        var data2 = new google.visualization.arrayToDataTable(data12);
 
+
+//SELECT teacher,DATEDIFF(`date_finished`,`date_approved`) as weeks FROM `projects` WHERE status = 'complete' GROUP BY teacher
+
+//SE TI KATASTASH VRISKONTAI OI DIPLWMATIKES ANA EKSAMHNO
 <?php
 $servername = "localhost";
 $username = "root";
@@ -101,61 +102,112 @@ if ($conn->connect_error) {
 }
 
 $teacher = $_SESSION["username"];
-$sql2 = "SELECT teacher,AVG(grade) as avg From projects GROUP BY teacher ";
+$sql2 = "SELECT status,COUNT(*) as count From projects WHERE MONTH(date_approved)< 6 GROUP BY status ";
 $result2 = $conn->query($sql2);
+
+$sql3 = "SELECT status,COUNT(*) as count From projects WHERE MONTH(date_approved) >6 GROUP BY status ";
+$result3 = $conn->query($sql3);
 ?>
 
 
 
-                var data3 = new google.visualization.DataTable();
-                data3.addColumn('string', 'Status');
-                data3.addColumn('number', 'Percent');
+                        var data3 = new google.visualization.DataTable();
+                        data3.addColumn('string', 'Status');
+                        data3.addColumn('number', 'Number of Projects');
 
 <?php
 if ($result2->num_rows > 0) {
     // output data of   each row
     while ($row1 = $result2->fetch_assoc()) {
         ?>
-                        data3.addRows([
-                            ['<?php echo $row1["teacher"]; ?>', <?php echo (int) $row1["avg"]; ?>]
-
-                        ]);
+                                data3.addRows([
+        <?php if ($row1["status"] == 'approved' || $row1["status"] == 'ready' || $row1["status"] == 'complete') { ?>
+                                        ['<?php
+            echo $row1["status"];
+            echo",Εαρινό";
+            ?>', <?php echo $row1["count"]; ?>]
+        <?php } ?>
+                                ]);
+        <?php
+    }
+    while ($row2 = $result3->fetch_assoc()) {
+        ?>
+                                data3.addRows([
+        <?php if ($row2["status"] == 'approved' || $row2["status"] == 'ready' || $row2["status"] == 'complete') { ?>
+                                        ['<?php
+            echo $row2["status"];
+            echo",Χειμερινό";
+            ?>', <?php echo $row2["count"]; ?>]
+        <?php } ?>
+                                ]);
         <?php
     }
 }
 ?>
-                // Set chart options
-                var options = {'title': 'Ο διπλωματικές μου σε σχέση με τους άλλους.',
-                    'width': 600,
-                    'height': 500};
-                // Set chart options
-                var options2 = {'title': 'Περισσότερες διπλωματικές ανά έτος και συνολικά',
-                    'width': 600,
-                    'height': 500};
-                // Set chart options
-                var options3 = {'title': 'Ποσοστά Διπλωματικών μου ανά Κατηγορία',
-                    'width': 600,
-                    'height': 500};
-                // Instantiate and draw our chart, passing in some options.
-                var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-                chart.draw(data, options);
-                var chart2 = new google.visualization.BarChart(document.getElementById('chart_div2'));
-                chart2.draw(data2, options2);
-                var chart3 = new google.visualization.BarChart(document.getElementById('chart_div3'));
-                chart3.draw(data3, options3);
 
 
-                var options4 = {
-                    chart: {
-                        title: 'Company Performance',
-                        subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+<?php
+$servername = "localhost";
+$username = "root";
+$dbname = "webdev";
+$conn = new mysqli($servername, $username, '', $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql5 = "SELECT teacher,SUM(DATEDIFF(date_finished,date_approved)/7) as week ,COUNT(*) as num  FROM projects WHERE status = 'complete' GROUP BY teacher";
+$result5 = $conn->query($sql5);
+
+$sql6 = "SELECT COUNT(*) as num, teacher FROM projects WHERE status = 'complete' GROUP BY teacher";
+$result6 = $conn->query($sql6);
+?>
+
+
+
+                        var data4 = new google.visualization.DataTable();
+                        data4.addColumn('string', 'Status');
+                        data4.addColumn('number', 'AVG Number of Weeks  ');
+
+<?php
+if ($result5->num_rows > 0) {
+    // output data of   each row
+    while ($row1 = $result5->fetch_assoc() ) {
+        ?>
+                                data4.addRows([
+                                    ['<?php echo $row1["teacher"]; ?>', <?php echo (int) ($row1["week"]/$row1["num"]); ?>],
+                                ]);
+        <?php
+    }
+}
+?>
+
+                        // Set chart options
+                        var options = {'title': 'Ο μέσος όρος των βαθμολογιών των πτυχιακών ανά καθηγητή.',
+                            'width': 600,
+                            'height': 500};
+                        // Set chart options
+                        var options2 = {'title': 'Περισσότερες διπλωματικές ανά έτος και συνολικά',
+                            'width': 600,
+                            'height': 500};
+                        // Set chart options
+                        var options3 = {'title': 'Σε τι κατάσταση βρίσκονται οι διπλωματικές ανά ακαδημιακό έτος',
+                            'width': 600,
+                            'height': 500};
+                        var options4 = {'title': 'Μέσος χρόνος ολοκλήρωσης διπλωματικών ( σε εβδομάδες) ανά καθηγητή',
+                            'width': 600,
+                            'height': 500};
+                        // Instantiate and draw our chart, passing in some options.
+                        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+                        chart.draw(data, options);
+                        var chart2 = new google.visualization.BarChart(document.getElementById('chart_div2'));
+                        chart2.draw(data2, options2);
+                        var chart3 = new google.visualization.BarChart(document.getElementById('chart_div3'));
+                        chart3.draw(data3, options3);
+                        var chart4 = new google.visualization.BarChart(document.getElementById('chart_div4'));
+                        chart4.draw(data4, options4);
+
                     }
-                };
-
-                var chart4 = new google.charts.Bar(document.getElementById('columnchart_material'));
-
-                chart4.draw(data2, google.charts.Bar.convertOptions(options4));
-            }
         </script>
     </head>
 
@@ -163,10 +215,11 @@ if ($result2->num_rows > 0) {
 
 
 
-        <div id="chart_div"></div>
+
         <div id="chart_div2"></div>
-        <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+        <div id="chart_div"></div>
         <div id="chart_div3"></div>
+        <div id="chart_div4"></div>
         <div class="container">
             <div class="col-md-12" style="padding:3%">
 
