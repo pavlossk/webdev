6c4ee3d475204ddd9183680ee7faa2d08259acba
georgezervas17 <icsd13055@icsd.aegean.gr>
2017-05-30 15:22:49 +0300

    stages

--- WebDev/insert_project.php
+++ WebDev/insert_project.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <?php session_start(); ?>
 <html lang="en">
-
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-        <title>WebDev - Insert Project</title>
+        <title>WebDev</title>
         <link rel="icon" href="img/trasp.png">
 
         <meta name="description" content="Your Description Here">
@@ -17,19 +15,19 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
         <!-- Favicons
-            ================================================== -->
+        ================================================== -->
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
         <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
         <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
         <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
 
         <!-- Bootstrap -->
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
 
         <!-- Stylesheet
-            ================================================== -->
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        ================================================== -->
+        <link rel="stylesheet" type="text/css"  href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/responsive.css">
 
         <script type="text/javascript" src="js/modernizr.custom.js"></script>
@@ -37,10 +35,10 @@
 
         <link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700,100,800,900,400,200,300' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
     </head>
     <script type="text/javascript">
         function checkForm(form) {
@@ -108,38 +106,46 @@
             if (!empty($_POST["ready"])) {
 
                 $servername = "localhost";
-                $username = "root@localhost";
+                $username = "root";
                 $dbname = "webdev";
 
                 $conn = new mysqli($servername, $username, '', $dbname);
-                // Check connection
                 if ($conn->connect_error) {
                     die("Connection failed: " . $conn->connect_error);
                 }
 
                 $title = $_POST["title"];
                 $teacher = $_POST["teacher"];
-                $students = $_POST["students"];
-                $aim = $_POST["aim"];
+                $students = (int) $_POST["studentsnumber"];
                 $summary = $_POST["summ"];
-                $lessons = $_POST["lessons"];
-                $knowledge = $_POST["knowledge"];
-                $date_publish = $_POST["date_publish"];
-                $date_take = $_POST["date_take"];
-                $grade_ = $_POST["grade"];
+
+
+
+                if (!empty($_POST["1"])) {
+                    $cloud = $_POST["1"];
+                } if (!empty($_POST["2"])) {
+                    $datamining = $_POST["2"];
+                }if (!empty($_POST["3"])) {
+                    $java = $_POST["3"];
+                }if (!empty($_POST["4"])) {
+                    $diktua = $_POST["4"];
+                } if (!empty($_POST["5"])) {
+                    $asfaleia = $_POST["5"];
+                }
 
 
 
 
-                $sql = "INSERT INTO projects(teacher,projectname,summary) VALUES($teacher', '$title', '$summary')";
 
+                $sql = "INSERT INTO projects(projectID, teacher, studentsnumber, status, projectname, summary, date_creation) VALUES(10,'$teacher','$students','$title','not applied'.'$summary',CURRENT_TIMESTAMP)";
                 if (mysqli_query($conn, $sql)) {
-
                     echo "Record updated successfully";
+                    $message = "Η αίτηση σου καταχωρήθηκε.";
+                    echo "<script type='text/javascript'>alert('$message'); window.location.href = '/webdev/WebDev/teacher_menu.php';</script>";
+                    
                 } else {
                     echo "Error updating record: " . mysqli_error($conn);
                 }
-
                 mysqli_close($conn);
             }
             ?>
@@ -150,36 +156,52 @@
                     <h3 style=" text-align: center; font-size:45px;">Εισαγωγή Διπλωματικής</h3>
                     <ul style="list-style-type:none; align-content:center; ">
 
-                        <form onsubmit="return checkForm(this);" method="post">
+                        <form id="" action="" method="post">
                             <h3 style="font-size:20px; font:bold;">Τίτλος: </h3>
                             <input style="width:300px;" type="text" name="title">
 
                             <h3 style="font-size:20px; font:bold;">Καθηγητής: </h3>
-                            <input style="width:300px;" type="text" style="width:300px;" name="teacher">
+                            <input style="width:300px;" type="text" style="width:300px;" name="teacher" value="<?php echo $_SESSION["username"]; ?>">
 
                             <h3 style="font-size:20px; font:bold;">Αριθμός Φοιτητών:</h3>
-                            <input style="width:300px;" type="number" name="students">
+                            <input style="width:300px;" type="number" name="studentsnumber">
 
                             <h3 style="font-size:20px; font:bold;">Στόχος Διπλωματικής: </h3>
                             <input style="width:300px;" type="text" name="aim">
 
                             <h3 style="font-size:20px; font:bold;">Περίληψη:</h3>
                             <input style="width:300px;" type="text" name="summ">
+                            <br>
+                            <input class="button5" data-toggle="collapse" data-target="#demo1" value="Προαπαιτούμενα Μαθήματα"> 
+                            <div id="demo1" class="collapse">
+                                <?php
+                                $servername = "localhost";
+                                $username = "root";
+                                $dbname = "webdev";
 
-                            <h3 style="font-size:20px; font:bold;">Προαπαιτούμενα μαθήματα:</h3>
-                            <input style="width:300px;" type="text" name="lessons">
+                                $conn = new mysqli($servername, $username, '', $dbname);
+                                if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                                }
+
+                                $sql = "SELECT * FROM lessons ";
+                                $result = $conn->query($sql);
+
+                                $lessons_number = $result->num_rows;
+
+                                if ($result->num_rows > 0) {
+                                    // output data of   each row
+                                    while ($row = $result->fetch_assoc()) {
+                                        ?>
+                                        <input type="checkbox" name="<?php echo $row["lessonID"]; ?>" value="<?php echo $row["lessonname"]; ?>"> <?php echo $row["lessonname"]; ?> <br>
+                                        <?php
+                                    }
+                                }
+                                ?>              
+                            </div>
 
                             <h3 style="font-size:20px; font:bold;">Προαπαιτούμενες γνώσεις:</h3>
-                            <input style="width:300px;" type="text" name="knowledge">
-
-                            <h3 style="font-size:20px; font:bold;">Ημερομηνία Δημοσιοποίησης:</h3>
-                            <input style="width:300px;" type="date" name="date_publish">
-
-                            <h3 style="font-size:20px; font:bold;">Ημερομηνία Ανάληψης:</h3>
-                            <input style="width:300px;" type="date" name="date_take">
-
-                            <h3 style="font-size:20px; font:bold;">Βαθμός:</h3>
-                            <input style="width:300px;" type="number" name="grade">
+                            <input style="width:300px;" type="text" name="knowledge" value="">
 
                             <br>
                             <br>
